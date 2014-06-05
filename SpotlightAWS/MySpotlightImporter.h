de260918041aa3f9a7639844b4514af58c324c6d
Chris Moyer <cmoyer@newstex.com>
2014-06-05 09:50:37 -0400

    Initial Commit

---
+++ SpotlightAWS/MySpotlightImporter.h
@@ -0,0 +1,19 @@
+//
+//  MySpotlightImporter.h
+//  SpotlightAWS
+//
+//  Created by Chris Moyer on 6/5/14.
+//  Copyright (c) 2014 Chris Moyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MySpotlightImporter : NSObject
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (BOOL)importFileAtPath:(NSString *)filePath attributes:(NSMutableDictionary *)attributes error:(NSError **)error;
+
+@end
